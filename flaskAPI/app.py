a1ccac2139607c3865ef2c223d0917504e74f924
unclestay <unclestay@163.com>
2020-01-30 13:21:04 +0100

    Update app.py

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash
 
 from function import get_hyperparametres, get_algorithm, get_score, X_train, X_test, Y_train, Y_test
@@ -17,10 +18,9 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 class MyForm(Form):
     n_estimators = StringField(label="n_estimators")
-    #max_features = StringField(label="max_features")
-    #max_depth = StringField(label="max_depth")
+    max_features = StringField(label="max_features")
+    max_depth = StringField(label="max_depth")
     random_forest = SubmitField(label="random_forest",validators=[DataRequired()])
-
 
 
 @app.route('/index', methods=['POST', 'GET'])
@@ -31,10 +31,10 @@ def inputParameters():
         if form.validate_on_submit():
             if form.random_forest.data:
                 n_estimators = request.form['n_estimators']
-                #max_features = request.form["max_features"]
-                #max_depth = request.form["max_depth"]
+                max_features = request.form["max_features"]
+                max_depth = request.form["max_depth"]
                 print("678")
-                return redirect(url_for('getNewModel', n_estimators=n_estimators))
+                return redirect(url_for('getNewModel', n_estimators=n_estimators, max_features=max_features, max_depth=max_depth))
     print("123")
     return render_template("index.html", form=form)
 
@@ -46,10 +46,11 @@ def getAccuracy():
     return render_template('accuracy.html', accuracy=accuracy)
 
 
-@app.route('/getNewModel/<int:n_estimators>', methods=["POST", "GET"])
-def getNewModel(n_estimators):
-    algorithm = get_algorithm(n_estimators, 9, 10)
-    return render_template('getNewModel.html', algorithm=algorithm)
+@app.route('/getNewModel/<int:n_estimators>/<int:max_features>/<int:max_depth>', methods=["POST", "GET"])
+def getNewModel(n_estimators, max_features, max_depth):
+    algorithm = get_algorithm(n_estimators, max_features, max_depth)
+    accuracy = get_score(algorithm, X_train, X_test, Y_train, Y_test)
+    return render_template('getNewModel.html', algorithm=algorithm, accuracy=accuracy)
 
 
 if __name__ == '__main__':
